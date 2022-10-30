c56075e331c1f56dd59f40c69adf1aae3c5b95e6
Rus <Rus@DESKTOP-H1L59SG>
2022-10-30 15:53:27 +0200

    Add project files.

---
+++ Rus_Ovidiu_Lab2/Data/Rus_Ovidiu_Lab2Context.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Rus_Ovidiu_Lab2.Models;
+
+namespace Rus_Ovidiu_Lab2.Data
+{
+    public class Rus_Ovidiu_Lab2Context : DbContext
+    {
+        public 
+        Rus_Ovidiu_Lab2Context (DbContextOptions<Rus_Ovidiu_Lab2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Rus_Ovidiu_Lab2.Models.Book> Book { get; set; } = default!;
+
+        public DbSet<Rus_Ovidiu_Lab2.Models.Publisher> Publisher { get; set; }
+    }
+}
