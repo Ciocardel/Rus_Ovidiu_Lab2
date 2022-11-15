cdfb6e26a217417966e13531b22ee277d2fce8e1
Ciocardel <ruso41842@gmail.com>
2022-11-15 22:51:51 +0200

    text

---
+++ Rus_Ovidiu_Lab2/Pages/Borrowings/Details.cshtml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Rus_Ovidiu_Lab2.Data;
+using Rus_Ovidiu_Lab2.Models;
+
+namespace Rus_Ovidiu_Lab2.Pages.Borrowings
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly Rus_Ovidiu_Lab2.Data.Rus_Ovidiu_Lab2Context _context;
+
+        public DetailsModel(Rus_Ovidiu_Lab2.Data.Rus_Ovidiu_Lab2Context context)
+        {
+            _context = context;
+        }
+
+      public Borrowing Borrowing { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null || _context.Borrowing == null)
+            {
+                return NotFound();
+            }
+
+            var borrowing = await _context.Borrowing.FirstOrDefaultAsync(m => m.ID == id);
+            if (borrowing == null)
+            {
+                return NotFound();
+            }
+            else 
+            {
+                Borrowing = borrowing;
+            }
+            return Page();
+        }
+    }
+}
